c198508d4ac0a09da9e4d4b4d627f2941ead8b8c
Rosangela <96451367+Rommendes@users.noreply.github.com>
2025-04-25 16:46:59 -0300

    atualizando

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext.jsx";
+import { AuthProvider } from "./Frontend/src/context/AuthContext.jsx";
 import ReactDOM from "react-dom/client";
 import './index.css'
-import App from './App.jsx'
+import App from './Frontend/src/App.jsx'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
